642f5e8d75ff31fd70d8b49fe3d046eff12ff4db
arvicco <arvitallian@gmail.com>
2010-09-06 12:28:56 +0400

    Initial commit to remote_logger.

---
+++ spec/remote_logger_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+module RemoteLoggerTest
+  describe RemoteLogger do
+  end
+end # module RemoteLoggerTest
+
---
+++ spec/spec_helper.rb
@@ -0,0 +1,18 @@
+require 'remote_logger'
+require 'spec'
+require 'spec/autorun'
+
+Spec::Runner.configure do |config|
+  # == Mock Framework
+  #
+  # RSpec uses it's own mocking framework by default. If you prefer to
+  # use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+end
+
+module RemoteLoggerTest
+
+end # module RemoteLoggerTest
